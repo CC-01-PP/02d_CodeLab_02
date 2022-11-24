b84517469f90164af59246f5853c1e27e96c0839
Mia Kamenetski <mia.kamenetski@online.de>
2022-11-24 14:46:29 +0100

    Mitarbeiter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,29 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+        Mitarbeiter mitarbeiter1 = new Mitarbeiter("Swift", "Taylor", "singer", 2004);
+        output(mitarbeiter1.getInfo("#lastName"));
+        output(mitarbeiter1.getInfo("#firstName"));
+        output(mitarbeiter1.getInfo("#role"));
+        output(mitarbeiter1.getInfo("#yearOfEntry"));
+
+        output("");
+        
+        Mitarbeiter mitarbeiter2 = new Mitarbeiter("Sheeran", "Ed", "Singer-songwriter", 2004);
+        output(mitarbeiter2.getInfo("#lastName"));
+        output(mitarbeiter2.getInfo("#firstName"));
+        output(mitarbeiter2.getInfo("#role"));
+        output(mitarbeiter2.getInfo("#yearOfEntry"));
+
+        output("");
+
+        Mitarbeiter mitarbeiter3 = new Mitarbeiter("John", "Elton", "Singer, Pianist, Composer and Philanthropist", 1962);
+        output(mitarbeiter3.getInfo("#lastName"));
+        output(mitarbeiter3.getInfo("#firstName"));
+        output(mitarbeiter3.getInfo("#role"));
+        output(mitarbeiter3.getInfo("#yearOfEntry"));
     }
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
---
+++ com/cc/java/Mitarbeiter.java
@@ -0,0 +1,33 @@
+package com.cc.java;
+
+public class Mitarbeiter {
+
+    private String lastName;
+    private String firstName;
+    private String role;
+    private int yearOfEntry;
+
+    public Mitarbeiter(String lastName, String firstName, String role, int yearOfEntry) {
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.role = role;
+        this.yearOfEntry = yearOfEntry;
+    }
+
+    public String getInfo(String flag) {
+        switch (flag) {
+            case "#lastName":
+                return lastName;
+            case "#firstName":
+                return firstName;
+            case "#role":
+                return role;
+            case "#yearOfEntry":
+                return Integer.toString(yearOfEntry);
+            default:
+                return "#!ERROR";
+        }
+    }
+
+    
+}
